dbdbbd42384923eb52f26f3c98f1775ce000b74b
LowLuck <cfif905@bk.ru>
2021-06-04 00:53:26 +0300

    Weather getting, but better

---
+++ UserWork.py
@@ -0,0 +1,29 @@
+from main import main
+
+try:
+    with open('user.txt', 'r') as file:
+        city = file.read()
+        print(f'If you want, you can change your city, just type chg \n'
+              f'If you dont want, type start to start')
+        w = input()
+
+        if w == 'start':
+            main(city)
+        elif w == 'chg':
+            city = input('Please, enter your city \n')
+            with open('user.txt', 'w') as file:
+                file.write(city)
+
+            w = input('Start? \n')
+            if w.lower() == 'start':
+                main(city)
+        else:
+            print('command not found')
+
+
+except FileNotFoundError:
+    city = input('Please, enter your city \n')
+    save = input('Should I save it (Y/N) \n')
+    if save == 'Y':
+        with open('user.txt', 'w') as file:
+            file.write('city')
--- main.py
+++ main.py
@@ -12,27 +12,20 @@
 
 import ctypes
 import requests
+
 SPI_SETDESKWALLPAPER = 20
 # ctypes.windll.user32.SystemParametersInfoW(20, 0, "E:\Testfolder\Jpic.png" , 0)
 
-try:
-    with open('user.txt', 'r') as file:
-        city = file.read()
 
-except FileNotFoundError:
-    city = input('Please, enter your city \n')
-    save = input('Should I save it (Y/N) \n')
-    if save == 'Y':
-        with open('user.txt', 'w') as file:
-            file.write('city')
+def main(city):
 
-
-api_key = 'p6gwoTEl8qSHNP9EFSj9fW8YRNvn0wA9'
-req = requests.get(f'http://dataservice.accuweather.com/locations/v1/cities/search?apikey={api_key}&q={city}').json()
-location_key = req[0]["Key"]
-print(location_key)
-req2 = requests.get(f'http://dataservice.accuweather.com/currentconditions/v1/{location_key}?apikey={api_key}').json()
-if req2:
-    print(req2[0]['WeatherText'])
-else:
-    print('err')
+    api_key = 'p6gwoTEl8qSHNP9EFSj9fW8YRNvn0wA9'
+    req = requests.get(
+        f'http://dataservice.accuweather.com/locations/v1/cities/search?apikey={api_key}&q={city}').json()
+    location_key = req[0]["Key"]
+    req2 = requests.get(
+        f'http://dataservice.accuweather.com/currentconditions/v1/{location_key}?apikey={api_key}').json()
+    if req2:
+        print(req2[0]['WeatherText'])
+    else:
+        print('Network Error...')
